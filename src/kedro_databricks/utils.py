caf6083b73e43a4ca305905f2e63ff109d975672
JensPederM <jenspederm@gmail.com>
2024-07-22 16:31:29 +0200

    fix bug where list contain only null values
    Signed-off-by: JensPederM <jenspederm@gmail.com>

--- src/kedro_databricks/utils.py
+++ src/kedro_databricks/utils.py
@@ -79,9 +79,16 @@ def _remove_nulls_from_dict(d: dict[str, Any]) -> dict[str, float | int | str | 
         if isinstance(v, dict):
             _remove_nulls_from_dict(v)
         elif isinstance(v, list):
+            new_values = []
             for item in v:
                 if isinstance(item, dict):
                     _remove_nulls_from_dict(item)
+
+                if item is None:
+                    continue
+
+                new_values.append(item)
+            d[k] = new_values
 
         if v is None:
             del d[k]
