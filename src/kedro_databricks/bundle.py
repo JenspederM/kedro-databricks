349bcadaeb4d4e513b3670f409818128f4a408cd
Jens Peder Meldgaard <jenspederm@gmail.com>
2024-07-25 13:34:33 +0200

    task keys cannot contain '.'
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

--- src/kedro_databricks/bundle.py
+++ src/kedro_databricks/bundle.py
@@ -30,9 +30,9 @@ def _create_task(name: str, depends_on: list[node], package: str) -> dict[str, A
     ## https://docs.databricks.com/api/workspace/jobs/create
 
     task = {
-        "task_key": name,
+        "task_key": name.replace(".", "_"),
         "libraries": [{"whl": "../dist/*.whl"}],
-        "depends_on": [{"task_key": dep.name} for dep in depends_on],
+        "depends_on": [{"task_key": dep.name.replace(".", "_")} for dep in depends_on],
         "python_wheel_task": {
             "package_name": package,
             "entry_point": "databricks_run",
