91c04ee41351edeaeb186b704a5a6dbf5c81cfb6
Jens Peder Meldgaard <jenspederm@gmail.com>
2025-03-16 20:07:12 +0100

    test: used parameterized tests
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -7,14 +7,15 @@ import subprocess
 
 import pytest
 
-from kedro_databricks.utils.common import (
-    KEDRO_VERSION,
-    Command,
+from kedro_databricks.constants import OVERRIDE_KEY_MAP
+from kedro_databricks.utils.bundle_helpers import (
+    _is_null_or_empty,
     get_entry_point,
-    make_workflow_name,
+    remove_nulls,
     require_databricks_run_script,
     sort_dict,
 )
+from kedro_databricks.utils.common import Command, make_workflow_name
 from kedro_databricks.utils.create_target_configs import _substitute_file_path
 from kedro_databricks.utils.has_databricks import has_databricks_cli
 from kedro_databricks.utils.override_resources import (
@@ -23,7 +24,6 @@ from kedro_databricks.utils.override_resources import (
     _update_list_by_key,
     override_resources,
 )
-from kedro_databricks.utils.remove_nulls import _is_null_or_empty, remove_nulls
 
 
 @pytest.mark.skipif(
@@ -42,25 +42,38 @@ def test_fail_with_python_databricks_cli():
     subprocess.run(["uv", "pip", "uninstall", "databricks-cli"], check=True)
 
 
-def test_unknown_lookup_key():
-    with pytest.raises(ValueError):
-        _get_lookup_key("unknown")
+@pytest.mark.parametrize(
+    ["key", "expected"],
+    [
+        ("unknown", None),
+        (123, None),
+        *[(key, value) for key, value in OVERRIDE_KEY_MAP.items()],
+    ],
+)
+def test_get_lookup_key(key, expected):
+    if expected is None:
+        with pytest.raises(ValueError):
+            _get_lookup_key(key)
+        return
+    result = _get_lookup_key(key)
+    assert result == expected
 
 
-def test_workflow_not_dict():
-    with pytest.raises(ValueError):
-        override_resources(
-            {"resources": {"jobs": {"workflow": "not_dict"}}}, {}, "default"
-        )
-
-
-def test_overrides_not_dict():
-    with pytest.raises(ValueError):
-        override_resources(
-            {"resources": {"jobs": {"workflow": {}}}},
-            [],  # type: ignore
-            "default",
-        )
+@pytest.mark.parametrize(
+    ["jobs", "overrides", "expected", "error"],
+    [
+        ({"workflow": "not_dict"}, {}, None, ValueError),
+        ({"workflow": {}}, [], None, ValueError),
+    ],
+)
+def test_override_resources(jobs, overrides, expected, error):
+    resources = {"resources": {"jobs": jobs}}
+    if error:
+        with pytest.raises(error):
+            override_resources(resources, overrides, "default")
+    else:
+        result = override_resources(resources, overrides, "default")
+        assert result == expected
 
 
 @pytest.mark.parametrize(
@@ -98,167 +111,186 @@ def test_substitute_file_path(actual, expected):
     assert result == expected, f"\n{result}\n{expected}"
 
 
-def test_command_fail_default():
-    with pytest.raises(Exception) as e:
-        cmd = ["ls", "non_existent_file"]
-        Command(cmd).run()
-        assert "Failed to run command" in str(
-            e.value
-        ), f"Failed to raise exception: {cmd}"
-        raise Exception(e)
+@pytest.mark.parametrize(
+    ["cmd", "result_code", "msg", "warn", "raises"],
+    [
+        (["ls", "."], 0, "", False, False),
+        (["ls", "non_existent_file"], 1, "Custom message", False, True),
+        (["ls", "non_existent_file"], 1, "Custom message", True, False),
+        (["ls", "non_existent_file"], 1, "", False, True),
+        (["ls", "non_existent_file"], 1, "", True, False),
+    ],
+)
+def test_command(cmd, result_code, warn, msg, raises):
+    if raises and not warn:
+        with pytest.raises(Exception) as e:
+            if msg:
+                result = Command(cmd, msg=msg).run()
+                assert msg in str(e.value)
+            else:
+                result = Command(cmd).run()
+            assert isinstance(result, subprocess.CompletedProcess)
+            assert result.returncode == result_code, result
+    else:
+        command = Command(cmd, warn=warn)
+        assert repr(command) == f"Command({cmd})", repr(command)
+        result = command.run()
+        assert isinstance(result, subprocess.CompletedProcess)
+        assert result.returncode == result_code, result
 
 
-def test_command_fail_custom():
-    with pytest.raises(Exception) as e:
-        cmd = ["ls", "non_existent_file"]
-        msg = "Custom message"
-        Command(cmd, msg=msg).run()
-        assert "Custom message" in str(e.value), f"Failed to raise exception: {cmd}"
-        raise Exception(e)
-
-
-def test_command_success():
-    result = Command(["ls", "."]).run()
-    assert isinstance(result, subprocess.CompletedProcess)
-
-
-def test_command_success_custom():
-    result = Command(["ls", "."], msg="Custom message").run()
-    assert isinstance(result, subprocess.CompletedProcess)
-
-
-def test_command_warn_default():
-    result = Command(["ls", "non_existent_file"], msg="Custom message", warn=True).run()
-    assert result.returncode != 0, result
-
-
-def test_command_warn_customz():
-    result = Command(["ls", "non_existent_file"], warn=True).run()
-    assert result.returncode != 0, result
-
-
-def test_command_repr():
-    cmd = Command(["ls", "."])
-    assert repr(cmd) == "Command(['ls', '.'])", repr(cmd)
-
-
-def test_get_entry_point():
-    tests = [
+@pytest.mark.parametrize(
+    ["actual", "expected"],
+    [
         ("Fake Project", "fake-project"),
         ("Fake Project 123", "fake-project"),
         ("Fake Project 123 456", "fake-project"),
         ("Fake Project #%", "fake-project"),
         ("# Fake Project #%", "fake-project"),
-    ]
-    for project_name, expected in tests:
-        entry_point = get_entry_point(project_name)
-        assert entry_point == expected, entry_point
+    ],
+)
+def test_get_entry_point(actual, expected):
+    entry_point = get_entry_point(actual)
+    assert entry_point == expected, entry_point
 
 
-def test_update_list():
-    old = [
-        {"task_key": "task1", "job_cluster_key": "cluster1"},
-        {"task_key": "task2", "job_cluster_key": "cluster2"},
-        {"task_key": "task3", "job_cluster_key": "cluster3"},
-    ]
-    new = [
-        {"task_key": "task1", "job_cluster_key": "cluster4"},
-    ]
-    result = _update_list_by_key(old, new, "task_key")
-    assert result == [
-        {"task_key": "task1", "job_cluster_key": "cluster4"},
-        {"task_key": "task2", "job_cluster_key": "cluster2"},
-        {"task_key": "task3", "job_cluster_key": "cluster3"},
-    ], result
+@pytest.mark.parametrize(
+    ["old", "new", "key", "default", "expected"],
+    [
+        ([], [], "task_key", {}, []),
+        ([], [], "task_key", {"job_cluster_key": "cluster1"}, []),
+        (
+            [
+                {"task_key": "task1", "job_cluster_key": "cluster1"},
+                {"task_key": "task2", "job_cluster_key": "cluster2"},
+                {"task_key": "task3", "job_cluster_key": "cluster3"},
+            ],
+            [
+                {"task_key": "task1", "job_cluster_key": "cluster4"},
+            ],
+            "task_key",
+            {},
+            [
+                {"task_key": "task1", "job_cluster_key": "cluster4"},
+                {"task_key": "task2", "job_cluster_key": "cluster2"},
+                {"task_key": "task3", "job_cluster_key": "cluster3"},
+            ],
+        ),
+        (
+            [
+                {"task_key": "task1"},
+                {"task_key": "task2"},
+                {"task_key": "task3"},
+            ],
+            [
+                {"task_key": "task1", "job_cluster_key": "cluster4"},
+            ],
+            "task_key",
+            {"job_cluster_key": "cluster1"},
+            [
+                {"task_key": "task1", "job_cluster_key": "cluster4"},
+                {"task_key": "task2", "job_cluster_key": "cluster1"},
+                {"task_key": "task3", "job_cluster_key": "cluster1"},
+            ],
+        ),
+    ],
+)
+def test_update_list(old, new, key, default, expected):
+    result = _update_list_by_key(old, new, key, default)
+    assert result == expected, result
 
 
-def test_update_list_default():
-    old = [
-        {"task_key": "task1"},
-        {"task_key": "task2"},
-        {"task_key": "task3"},
-    ]
-    new = [
-        {"task_key": "task1", "job_cluster_key": "cluster4"},
-    ]
-    result = _update_list_by_key(old, new, "task_key", {"job_cluster_key": "cluster1"})
-    assert result == [
-        {"task_key": "task1", "job_cluster_key": "cluster4"},
-        {"task_key": "task2", "job_cluster_key": "cluster1"},
-        {"task_key": "task3", "job_cluster_key": "cluster1"},
-    ], result
+@pytest.mark.parametrize(
+    ["actual", "order", "expected"],
+    [
+        (
+            {
+                "c": 1,
+                "a": 2,
+                "b": 3,
+            },
+            ["a", "b", "c"],
+            {
+                "a": 2,
+                "b": 3,
+                "c": 1,
+            },
+        ),
+        (
+            {
+                "a": 1,
+                "b": 2,
+                "c": 3,
+            },
+            ["c", "b", "a"],
+            {
+                "c": 3,
+                "b": 2,
+                "a": 1,
+            },
+        ),
+    ],
+)
+def test_sort_dict(actual, order, expected):
+    result = sort_dict(actual, order)
+    assert result == expected, result
 
 
-def test_sort_dict():
-    old = {
-        "c": 1,
-        "a": 2,
-        "b": 3,
-    }
-    result = sort_dict(old, ["a", "b", "c"])
-
-    assert result == {
-        "a": 2,
-        "b": 3,
-        "c": 1,
-    }, result
+@pytest.mark.parametrize(
+    ["value", "expected"],
+    [
+        ([0, 19, 8], False),
+        ([0, 19, 6], True),
+    ],
+)
+def test_require_databricks_run_script(value, expected):
+    assert require_databricks_run_script(value) == expected, value
 
 
-def test_require_databricks_run_script():
-    assert not require_databricks_run_script(
-        [0, 19, 8]
-    ), f"Should NOT require run script - {KEDRO_VERSION}"
-
-
-def test_require_databricks_run_script_fail():
-    assert require_databricks_run_script(
-        [0, 19, 6]
-    ), f"Should require run script - {KEDRO_VERSION}"
-
-
-def test_make_workflow_name():
-    tests = [
+@pytest.mark.parametrize(
+    ["package_name", "pipeline_name", "expected"],
+    [
         ("package", "__default__", "package"),
         ("package", "pipeline", "package_pipeline"),
-    ]
-    for package_name, pipeline_name, expected in tests:
-        workflow_name = make_workflow_name(package_name, pipeline_name)
-        assert workflow_name == expected, workflow_name
+    ],
+)
+def test_make_workflow_name(package_name, pipeline_name, expected):
+    workflow_name = make_workflow_name(package_name, pipeline_name)
+    assert workflow_name == expected, workflow_name
 
 
-def test_remove_nulls_from_dict():
-    a = {
-        "a": 1,
-        "b": None,
-        "c": {"d": None},
-        "e": {"f": {"g": None}},
-        "h": {"i": {"j": {}}},
-        "k": [],
-        "l": [1, 2, 3],
-        "m": [1, None, 3],
-        "n": [1, {"o": None}, 3],
-        "o": [1, [None], 3],
-        "p": [1, {"q": {"r": None}}, 3],
-    }
-
-    assert remove_nulls(a) == {
-        "a": 1,
-        "l": [1, 2, 3],
-        "m": [1, 3],
-        "n": [1, 3],
-        "o": [1, 3],
-        "p": [1, 3],
-    }, remove_nulls(a)
+@pytest.mark.parametrize(
+    ["value", "expected"],
+    [
+        ([1, 2, 3], [1, 2, 3]),
+        ([1, None, 3], [1, 3]),
+        ([1, {"a": None}, 3], [1, 3]),
+        ([1, [None], 3], [1, 3]),
+        ([1, {"a": {"b": None}}, 3], [1, 3]),
+        ({}, {}),
+        ({"a": 1, "b": None}, {"a": 1}),
+        ({"a": 1, "b": {"c": None}}, {"a": 1}),
+        ({"a": 1, "b": {"c": {"d": None}}}, {"a": 1}),
+    ],
+)
+def test_remove_nulls_from_dict(value, expected):
+    assert remove_nulls(value) == expected
 
 
-def test_is_null_or_empty():
-    assert _is_null_or_empty(None), "Failed to check None"
-    assert _is_null_or_empty({}), "Failed to check empty dict"
-    assert _is_null_or_empty([]), "Failed to check empty list"
-    assert not _is_null_or_empty(1), "Failed to check int"
-    assert not _is_null_or_empty("a"), "Failed to check str"
-    assert not _is_null_or_empty({1: 1}), "Failed to check dict"
-    assert not _is_null_or_empty([1]), "Failed to check list"
+@pytest.mark.parametrize(
+    ["value", "expected"],
+    [
+        (None, True),
+        ({}, True),
+        ([], True),
+        (1, False),
+        ("a", False),
+        ({1: 1}, False),
+        ([1], False),
+    ],
+)
+def test_is_null_or_empty(value, expected):
+    assert _is_null_or_empty(value) == expected, value
 
 
 @pytest.mark.parametrize(
@@ -305,10 +337,14 @@ def test_override_workflow(dct, overrides, expected):
     assert result == expected, result
 
 
-def test_override_workflow_fail():
-    with pytest.raises(TypeError):
-        _override_workflow(None, {})  # type: ignore
-    with pytest.raises(AttributeError):
-        _override_workflow({}, None)  # type: ignore
-    with pytest.raises(AttributeError):
-        _override_workflow({}, None, None)  # type: ignore
+@pytest.mark.parametrize(
+    ["args", "error"],
+    [
+        ([None, {}], TypeError),
+        ([{}, None], AttributeError),
+        ([{}, None, None], AttributeError),
+    ],
+)
+def test_override_workflow_fail(args, error):
+    with pytest.raises(error):
+        _override_workflow(*args)
