ebc01788f0c11565c528311feab3017884efbe0b
Jens Peder Meldgaard <jenspederm@gmail.com>
2025-06-13 17:02:09 +0200

    docs: generate markdown for examples
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

---
+++ scripts/gen_example_pages.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+import mkdocs_gen_files
+
+mkdocs_gen_files.log.info("Generating example pages...")
+root = Path(__file__).parent.parent
+examples_dir = root / "examples"
+
+
+def gen():
+    """Generate the code reference pages."""
+    for p in sorted(examples_dir.iterdir()):
+        mkdocs_gen_files.log.info(f"Processing example: {p.name}")
+        example_name = p.name
+        if not p.is_dir() or example_name.startswith("."):
+            continue
+
+        parts = [
+            f"# {example_name.title().replace('_', ' ')}",
+            "",
+            "This page contains the Databricks configuration and resources for the example.",
+            "",
+            '=== "conf/[env]/databricks.yml"',
+            "    ```yaml",
+            *[
+                "    " + line
+                for line in (p / "databricks.yml").read_text().strip().splitlines()
+            ],
+            "    ```",
+            "",
+            '=== "Before: resources/<pipeline>.yml"',
+            "    ```yaml",
+            *[
+                "    " + line
+                for line in (p / "resources.yml").read_text().strip().splitlines()
+            ],
+            "    ```",
+            "",
+            '=== "After: resources/<pipeline>.yml"',
+            "    ```yaml",
+            *[
+                "    " + line
+                for line in (p / "result.yml").read_text().strip().splitlines()
+            ],
+            "    ```",
+        ]
+        with mkdocs_gen_files.open(f"examples/{example_name}.md", "w") as f:
+            f.write("\n".join(parts) + "\n")
+    pass
+
+
+gen()
