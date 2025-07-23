47bfeff09cc27a15fa7534b81ed02d7e010235bc
Jens Peder Meldgaard <jenspederm@gmail.com>
2025-07-23 17:08:35 +0200

    chore: we also want to format
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

--- scripts/run_lint.sh
+++ scripts/run_lint.sh
@@ -6,3 +6,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 echo "$result"
+result=$(uv run ruff format .)
+if [ $? -ne 0 ]; then
+    echo "Lint failed"
+    echo "$result"
+    exit 1
+fi
+echo "$result"
