2f8786416bbaea461734262a7c1f7ebd6cf1a4a4
Jens Peder Meldgaard <jenspederm@gmail.com>
2025-12-08 08:57:18 +0100

    build: add type-checking pre-commit hook
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

--- scripts/gen_changelog.py
+++ scripts/gen_changelog.py
@@ -6,12 +6,22 @@ import mkdocs_gen_files
 
 mkdocs_gen_files.log.info("Generating changelog...")
 
+with tempfile.NamedTemporaryFile(suffix=".md", delete=False) as tmp:
+    tmp_path = Path(tmp.name)
+    subprocess.run(
+        [
+            "uv",
+            "run",
+            "cz",
+            "ch",
+            "--file-name",
+            tmp_path.as_posix(),
+        ],
+        check=False,
+    )
+    with mkdocs_gen_files.open("changelog.md", "w") as f:
+        f.write(tmp_path.read_text())
 
-tmp = Path(tempfile.mktemp()).with_suffix(".md")
-subprocess.run(["uv", "run", "cz", "ch", "--file-name", tmp.as_posix()], check=False)
-with mkdocs_gen_files.open("changelog.md", "w") as f:
-    f.write(tmp.read_text())
 mkdocs_gen_files.log.info("Changelog generated successfully.")
-
-tmp.unlink(missing_ok=True)  # Clean up the temporary file
+tmp_path.unlink(missing_ok=True)  # Clean up the temporary file
 mkdocs_gen_files.log.info("Temporary file cleaned up.")
