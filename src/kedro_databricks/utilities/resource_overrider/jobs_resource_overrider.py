ddb96d638d637ddabfea0be85ed5551647864df7
Jens Peder Meldgaard <JensPederM@gmail.com>
2026-04-20 08:21:34 +0200

    fix: adds deterministic ordering of object fields
    Signed-off-by: Jens Peder Meldgaard <JensPederM@gmail.com>

--- src/kedro_databricks/utilities/resource_overrider/jobs_resource_overrider.py
+++ src/kedro_databricks/utilities/resource_overrider/jobs_resource_overrider.py
@@ -12,6 +12,71 @@ from kedro_databricks.utilities.resource_overrider.abstract_resource_overrider i
 
 log = get_logger("JobsResourceOverrider")
 
+JOB_KEY_ORDER = [
+    "name",
+    "description",
+    "parameters",
+    "environments",
+    "job_clusters",
+    "tasks",
+    "access_control_list",
+    "budget_policy_id",
+    "continuous",
+    "deployment",
+    "edit_mode",
+    "email_notifications",
+    "format",
+    "git_source",
+    "health",
+    "max_concurrent_runs",
+    "notification_settings",
+    "performance_target",
+    "queue",
+    "run_as",
+    "schedule",
+    "tags",
+    "timeout_seconds",
+    "trigger",
+    "webhook_notifications",
+]
+
+TASK_KEY_ORDER = [
+    "task_key",
+    "description",
+    "depends_on",
+    "environment_key",
+    "job_cluster_key",
+    "existing_cluster_id",
+    "libraries",
+    "new_cluster",
+    "compute",
+    "disable_auto_optimization",
+    "health",
+    "run_if",
+    "max_retries",
+    "min_retry_interval_millis",
+    "retry_on_timeout",
+    "timeout_seconds",
+    "notification_settings",
+    "email_notifications",
+    "webhook_notifications",
+    "alert_task",
+    "clean_rooms_notebook_task",
+    "condition_task",
+    "dashboard_task",
+    "dbt_task",
+    "for_each_task",
+    "notebook_task",
+    "pipeline_task",
+    "power_bi_task",
+    "python_wheel_task",
+    "run_job_task",
+    "spark_jar_task",
+    "spark_python_task",
+    "spark_submit_task",
+    "sql_task",
+]
+
 
 def _notification_overrider(old, new):
     return create_merge_factory(
@@ -114,6 +179,7 @@ def _tasks_overrider(old: list[dict], new: list[dict], defaults: dict) -> list[d
                 ),
                 "libraries": _libraries_overrider,
             },
+            key_order=TASK_KEY_ORDER,
         )
         tasks.append(merged_task)
     return sorted(tasks, key=lambda x: x.get("task_key", ""))
@@ -204,7 +270,7 @@ class JobsResourceOverrider(AbstractResourceOverrider):
                     old, new, key="name"
                 ),
             },
-            key_order=[],
+            key_order=JOB_KEY_ORDER,
             post_processor=_post_processor,
         )
         all_overrides = {**default_overrides, **regex_overrides, **job_overrides}
